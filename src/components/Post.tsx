cf48289af5dc8ebf6a8f59092d0e082d677c4c71
hhkim0729 <heehkim@student.42seoul.kr>
2022-06-29 17:22:40 +0900

    add: Add Post component(#16)

---
+++ src/components/Post.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Post = () => {
+  return (
+    <div>
+      <div>
+        <div>이름(id)</div>
+        <div>날짜</div>
+      </div>
+      <div>
+        <div>내용</div>
+        <div>버튼</div>
+      </div>
+    </div>
+  );
+};
