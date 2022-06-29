695ffc563ae59c145840ad56bba296d3c579db36
hhkim0729 <heehkim@student.42seoul.kr>
2022-06-29 19:34:54 +0900

    add: Post 컴포넌트 추가(#16)
    아직 데이터를 받아오지 못하기 때문에 기본적인 껍데기만 구현

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import 'assets/css/Post.css';
 
 export const Post = () => {
   return (
-    <div>
-      <div>
-        <div>이름(id)</div>
-        <div>날짜</div>
+    <div className="Post">
+      <div className="top">
+        <div className="username">깃헙이름(githubId)</div>
+        <div className="date">날짜</div>
       </div>
-      <div>
-        <div>내용</div>
-        <div>버튼</div>
+      <div className="bottom">
+        <div className="content">처음 만들어 본 리액트 앱 리뷰 신청합니다.</div>
+        <div className="button-wrap">
+          <button className="button">리뷰하러 가기</button>
+        </div>
       </div>
     </div>
   );
