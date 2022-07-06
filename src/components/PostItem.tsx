7b52afbd5770dfde29ef5133fe0b9a046b0d3626
hhkim0729 <heehkim@student.42seoul.kr>
2022-07-06 21:05:16 +0900

    add: Post 컴포넌트에 props 추가 및 날짜 형식 지정(#16)

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
+import { PostItemType } from 'types/postTypes';
 import 'assets/css/PostItem.css';
 
-const PostItem = () => {
+function formatDate(rawDate: string) {
+  const rawDateObject = new Date(rawDate);
+  const todayDateObject = new Date();
+  const diffHour = Math.floor(
+    (todayDateObject.getTime() - rawDateObject.getTime()) / (1000 * 60 * 60)
+  );
+  const diffDate = Math.floor(diffHour / 24);
+  let dateString;
+  if (diffHour < 24) {
+    dateString = diffHour === 0 ? '방금 전' : `${diffHour}시간 전`;
+  } else if (diffDate <= 6) {
+    dateString = `${diffDate}일 전`;
+  } else {
+    const year = rawDateObject.getFullYear();
+    const month = rawDateObject.getMonth() + 1;
+    const date = rawDateObject.getDate();
+    const hour = rawDateObject.getHours();
+    const minute = rawDateObject.getMinutes();
+    const ampm = hour < 12 ? '오전' : '오후';
+    dateString = `${year}.${month}.${date} ${ampm} ${
+      hour > 12 ? hour % 12 : hour
+    }:${minute}`;
+  }
+  return dateString;
+}
+
+const PostItem = ({ id, pr_link, contents, created_at }: PostItemType) => {
+  const formattedDate = formatDate(created_at);
+
   return (
     <div className="PostItem">
       <div className="top">
         <div className="username">깃헙이름(githubId)</div>
-        <div className="date">날짜</div>
+        <div className="date">{formattedDate}</div>
       </div>
       <div className="bottom">
-        <div className="content">처음 만들어 본 리액트 앱 리뷰 신청합니다.</div>
+        <div className="content">{contents}</div>
         <div className="button-wrap">
           <button className="button">리뷰하러 가기</button>
         </div>
