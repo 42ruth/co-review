5c826820217bcb61c97607ed892a083f0987bfec
srngch <srngch.dev@gmail.com>
2022-07-06 20:04:28 +0900

    add: Form 컴포넌트 추가
    - 별도 이슈로 진행중인 자식 컴포넌트의 위치는 TODO로 주석 표기
    - 폼 안내를 위한 컴포넌트 별도로 작성

---
+++ src/pages/FormPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import FormNotice from 'components/Form/FormNotice';
+import Form from 'components/Form/Form';
+
+const FormPage = () => {
+  return (
+    <div>
+      <h2>리뷰 신청하기</h2>
+      <FormNotice />
+      <Form />
+    </div>
+  );
+};
+
+export default FormPage;
