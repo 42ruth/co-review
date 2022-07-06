96e3b2b1ce0146aa8c4dc9ddfc2792373923977f
humonnom <josojoso0702@gmail.com>
2022-07-06 21:13:56 +0900

    add: api 요청하는 함수 리턴값을 상의한 내용대로 바꿈

---
+++ src/api/useFetch.tsx
@@ -0,0 +1,67 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios, { AxiosResponse } from 'axios';
+import { useCallback, useState } from 'react';
+export interface ApiResponseType {
+  data: any;
+  status: number;
+}
+
+export interface FetchProps {
+  endpoint: string;
+  method: string;
+  data?: any;
+}
+
+export interface FetchDataType {
+  isLoading: boolean;
+  error: any | null;
+  data: any | null;
+  request: () => void;
+}
+
+export function useFetch({
+  endpoint,
+  method,
+  data,
+}: FetchProps): FetchDataType {
+  const [result, setResult] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const headers = {
+    Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+  };
+
+  const request = useCallback(() => {
+    setIsLoading(true);
+    let axiosPromise: Promise<AxiosResponse<ApiResponseType>> | null;
+    if (method === 'post') {
+      axiosPromise = axios.post(endpoint, data, { headers });
+    } else if (method === 'put') {
+      axiosPromise = axios.put(endpoint, data, { headers });
+    } else if (method === 'delete') {
+      axiosPromise = axios.delete(endpoint, { headers });
+    } else {
+      axiosPromise = axios.get(endpoint, {
+        headers,
+        params: data,
+      });
+    }
+
+    axiosPromise
+      ?.then((res: ApiResponseType) => {
+        console.log(res);
+        setResult(res.data.data);
+        setError(null);
+      })
+      ?.catch((err) => {
+        setResult(null);
+        setError(err);
+      })
+      ?.finally(() => {
+        setIsLoading(false);
+      });
+  }, [endpoint, method, data]);
+
+  return { isLoading, error, data: result, request };
+}
