0cd9f31721c1da38b7a6be1c357ce37dae5a6e8a
srngch <srngch.dev@gmail.com>
2022-07-12 16:58:53 +0900

    update: Post 컴포넌트 파일 위치 변경 및 PrLinkButton 컴포넌트
    - 디렉토리 자체가 컴포넌트와 이름이 같기 때문에 파일명을 index로 수정
    - PrLinkButton 컴포넌트를 import해서 적용함

---
+++ src/components/PostItem/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PrLinkButton from './PrLinkButton';
+import 'assets/css/Post.css';
+
+export const Post = () => {
+  return (
+    <div className="Post">
+      <div className="top">
+        <div className="username">깃헙이름(githubId)</div>
+        <div className="date">날짜</div>
+      </div>
+      <div className="bottom">
+        <div className="content">처음 만들어 본 리액트 앱 리뷰 신청합니다.</div>
+        <div className="button-wrap">
+          {/* TODO: prLink URL value from Post data*/}
+          <PrLinkButton prLink="#" />
+        </div>
+      </div>
+    </div>
+  );
+};
