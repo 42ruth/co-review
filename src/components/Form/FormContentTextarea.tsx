1446019d0ccf710066c4d6ea3b710549f0672e25
sujikim <sujikim@student.42seoul.kr>
2022-07-12 19:53:34 +0900

    fix: 프리티어 설정되지 않았던 코드 수정
    직전 작업한 코드에 프리티어 수정이 되어있지 않았습니다.

--- src/components/Form/FormContentTextarea.tsx
+++ src/components/Form/FormContentTextarea.tsx
@@ -1,29 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 interface FormContentTextareaProp {
-  value:string;
+  value: string;
   // eslint-disable-next-line
-  setValue: (value:string) => void;
+  setValue: (value: string) => void;
 }
 
-const maxContentLength = 300; 
+const maxContentLength = 300;
 
-const FormContentTextarea = ({value, setValue} : FormContentTextareaProp) => {
-  const [errorMessage, setErrorMessage] = useState("");
+const FormContentTextarea = ({ value, setValue }: FormContentTextareaProp) => {
+  const [errorMessage, setErrorMessage] = useState('');
   const handleChange = (event: React.FormEvent<HTMLTextAreaElement>) => {
     const { value } = event.currentTarget;
     setValue(value);
     if (!errorMessage && value.length >= maxContentLength)
-      setErrorMessage(`최대 ${maxContentLength}자까지만 입력 가능합니다.`)
+      setErrorMessage(`최대 ${maxContentLength}자까지만 입력 가능합니다.`);
     else if (errorMessage && value.length < maxContentLength)
-      setErrorMessage("");
-  }
+      setErrorMessage('');
+  };
   return (
     <>
-      <textarea value= {value} onChange= {handleChange} maxLength={maxContentLength} required/>
+      <textarea
+        value={value}
+        onChange={handleChange}
+        maxLength={maxContentLength}
+        required
+      />
       {errorMessage && <p>{errorMessage}</p>}
     </>
-  )
-}
+  );
+};
 
-export default FormContentTextarea
+export default FormContentTextarea;
