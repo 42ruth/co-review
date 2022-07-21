4303c64255fd80bcbe4a2dc5cbc8751218cb58fb
hhkim0729 <heehkim@student.42seoul.kr>
2022-07-21 15:53:39 +0900

    add: 메인에서 API를 통해 포스트 리스트를 조회(#58)

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,58 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useFetch, FetchDataType } from 'api/useFetch';
 import PostItem from 'components/PostList/PostItem';
+import { API_ORIGIN } from 'constants/';
+
+interface responseDataType {
+  id: number;
+  attributes: { prLink: string; contents: string; createdAt: string };
+}
 
 const PostList = () => {
-  const datas = [
-    {
-      id: 1,
-      prLink: 'https://github.com/42ruth/co-review/issues/10',
-      createdAt: '2022-06-30T05:20:47.363Z',
-      contents: 'Hello! This is a post to test.',
-    },
-    {
-      id: 4,
-      prLink: 'https://github.com/s2lululala/react-signup-form/pull/10',
-      createdAt: '2022-06-30T05:55:38.544Z',
-      contents: '회원가입 폼 코드리뷰 요청합니다. 수정 테스트.',
-    },
-    {
-      id: 7,
-      prLink: 'https://github.com/humonnom/react-practice-form/pull/25',
-      createdAt: '2022-07-06T11:40:10.304Z',
-      contents: '리뷰해주세요.',
-    },
-    {
-      id: 8,
-      prLink: 'https://github.com/humonnom/react-practice-form/pull/25',
-      createdAt: '2022-07-06T11:40:39.364Z',
-      contents: '리뷰해주세요.',
-    },
-    {
-      id: 9,
-      prLink: 'https://github.com/s2lululala/react-signup-form/pull/10',
-      createdAt: '2022-07-06T11:41:06.096Z',
-      contents: '회원가입 폼 코드리뷰 요청합니다.',
-    },
-    {
-      id: 11,
-      prLink: 'https://github.com/humonnom/react-practice-form/pull/25',
-      createdAt: '2022-07-06T11:41:27.969Z',
-      contents: '리뷰해주세요.',
-    },
-  ];
+  const { isLoading, error, data, request }: FetchDataType = useFetch({
+    endpoint: `${API_ORIGIN}/posts`,
+    method: 'get',
+  });
+
+  useEffect(() => {
+    request();
+  }, []);
+
   return (
     <section>
-      {datas.map((data, index) => {
-        return (
-          <PostItem
-            key={index}
-            id={data.id}
-            prLink={data.prLink}
-            contents={data.contents}
-            createdAt={data.createdAt}
-          />
-        );
-      })}
+      {isLoading && <div>Loading...</div>}
+      {!isLoading && error && <div>error...</div>}
+      {!isLoading &&
+        data &&
+        data.map((post: responseDataType, index: number) => {
+          return (
+            <PostItem
+              key={index}
+              id={post.id}
+              prLink={post.attributes.prLink}
+              contents={post.attributes.contents}
+              createdAt={post.attributes.createdAt}
+            />
+          );
+        })}
     </section>
   );
 };
