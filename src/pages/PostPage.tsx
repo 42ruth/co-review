0a43418dd358223b83062729c640388041cd86f3
hhkim0729 <heehkim@student.42seoul.kr>
2022-07-28 14:52:55 +0900

    add: PostPage 컴포넌트 추가(#79)

---
+++ src/pages/PostPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostDetail from 'components/PostDetail';
+
+const PostPage = () => {
+  return (
+    <main>
+      <h1>postItem</h1>
+      <PostDetail />
+    </main>
+  );
+};
+
+export default PostPage;
