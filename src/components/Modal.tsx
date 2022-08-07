49d60b6d5cc568eb682b6bdeb39706dd8409e701
hhkim0729 <heehkim@student.42seoul.kr>
2022-08-07 16:38:35 +0900

    add: DeleteButton  클릭 시 띄우는 Modal 컴포넌트 추가(#95)

---
+++ src/components/Modal.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import 'assets/css/Modal.css';
+
+interface ModalProp {
+  children: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+const Modal = ({ children, onConfirm, onCancel }: ModalProp) => {
+  useEffect(() => {
+    document.body.style.cssText = `overflow: hidden`;
+    return () => {
+      document.body.style.cssText = `overflow: auto`;
+    };
+  }, []);
+
+  return (
+    <div className="Modal">
+      <div className="box">
+        <div className="header">
+          <button onClick={onCancel}>❌</button>
+        </div>
+        <div className="content">{children}</div>
+        <div className="button-box">
+          <button className="button cancel" onClick={onCancel}>
+            취소
+          </button>
+          <button className="button confirm" onClick={onConfirm}>
+            확인
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
