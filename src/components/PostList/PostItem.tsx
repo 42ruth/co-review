f55937f67fa81694db40ae20d9848540944322d3
hhkim0729 <heehkim@student.42seoul.kr>
2022-08-12 15:39:37 +0900

    add: 포스트의 작성 시간을 실시간으로 업데이트 (#49)

--- src/components/PostList/PostItem.tsx
+++ src/components/PostList/PostItem.tsx
@@ -1,34 +1,9 @@
-import React from 'react';
-import PrLinkButton from 'components/PostList/PrLinkButton';
+import React, { useEffect, useRef, useState } from 'react';
 import { PostItemType } from 'types/postTypes';
+import { formatDate } from 'utils/dateUtil';
+import DeleteButton from 'components/PostList/DeleteButton';
+import PrLinkButton from 'components/PostList/PrLinkButton';
 import 'assets/css/PostItem.css';
-import DeleteButton from './DeleteButton';
-
-function formatDate(rawDate: string) {
-  const rawDateObject = new Date(rawDate);
-  const todayDateObject = new Date();
-  const diffHour = Math.floor(
-    (todayDateObject.getTime() - rawDateObject.getTime()) / (1000 * 60 * 60)
-  );
-  const diffDate = Math.floor(diffHour / 24);
-  let dateString;
-  if (diffHour < 24) {
-    dateString = diffHour === 0 ? '방금 전' : `${diffHour}시간 전`;
-  } else if (diffDate <= 6) {
-    dateString = `${diffDate}일 전`;
-  } else {
-    const year = rawDateObject.getFullYear();
-    const month = rawDateObject.getMonth() + 1;
-    const date = rawDateObject.getDate();
-    const hour = rawDateObject.getHours();
-    const minute = rawDateObject.getMinutes();
-    const ampm = hour < 12 ? '오전' : '오후';
-    dateString = `${year}.${month}.${date} ${ampm} ${
-      hour > 12 ? hour % 12 : hour
-    }:${minute}`;
-  }
-  return dateString;
-}
 
 interface PostItemProp extends PostItemType {
   refresh: () => void;
@@ -41,7 +16,15 @@ const PostItem = ({
   createdAt,
   refresh,
 }: PostItemProp) => {
-  const formattedDate = formatDate(createdAt);
+  const [formattedDate, setFormattedDate] = useState(formatDate(createdAt));
+  const interval: { current: NodeJS.Timeout | null } = useRef(null);
+
+  useEffect(() => {
+    interval.current = setInterval(() => {
+      setFormattedDate(formatDate(createdAt));
+    }, 1000 * 60);
+    return () => clearInterval(interval.current as NodeJS.Timeout);
+  }, []);
 
   return (
     <div className="PostItem">
---
+++ src/utils/dateUtil.tsx
@@ -0,0 +1,32 @@
+export function padNumber(num: number) {
+  return String(num).padStart(2, '0');
+}
+
+export function formatDate(rawDate: string) {
+  const rawDateObject = new Date(rawDate);
+  const todayDateObject = new Date();
+  const diffMin = Math.floor(
+    (todayDateObject.getTime() - rawDateObject.getTime()) / (1000 * 60)
+  );
+  const diffHour = Math.floor(diffMin / 60);
+  const diffDate = Math.floor(diffHour / 24);
+  let dateString;
+  if (diffMin < 60) {
+    dateString = diffMin === 0 ? '방금 전' : `${diffMin}분 전`;
+  } else if (diffHour < 24) {
+    dateString = `${diffHour}시간 전`;
+  } else if (diffDate < 7) {
+    dateString = `${diffDate}일 전`;
+  } else {
+    const year = rawDateObject.getFullYear();
+    const month = rawDateObject.getMonth() + 1;
+    const date = rawDateObject.getDate();
+    const hour = rawDateObject.getHours();
+    const minute = rawDateObject.getMinutes();
+    const ampm = hour < 12 ? '오전' : '오후';
+    dateString = `${year}.${padNumber(month)}.${padNumber(date)} ${ampm} ${
+      hour > 12 ? hour % 12 : hour
+    }:${padNumber(minute)}`;
+  }
+  return dateString;
+}
