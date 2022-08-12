7375e838da3a4b2ba8c9c983e617178b09d8f5dd
sujikim <sujikim@student.42seoul.kr>
2022-08-12 15:31:13 +0900

    add: 새로고침에도 로그인상태 유지되도록 로컬스토리지 저장
    react 상태및 context 전역 상태는 새로고침시 초기화됩니다.
    새로고침시에도 정보를 유지하기 위해 로컬스토리지에 저장하였습니다.
    로컬스토리지에 저장한 정보는 로그인/로그아웃 시도시 동기화합니다.
    
    Resolves #80

--- src/components/Auth/LogInOutButton.tsx
+++ src/components/Auth/LogInOutButton.tsx
@@ -1,12 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { userContext } from 'contexts/userContext';
 import { API_ORIGIN } from 'constants/index';
+
+const storage = window.localStorage;
+// 필요시 window.sessionStorage 등으로 변경 가능
 
 const LogInOutButton = () => {
   const auth = useContext(userContext);
 
+  useEffect(() => {
+    const storageString = storage.getItem('userState');
+
+    if (auth?.userState.isLogin) {
+      const contextString = JSON.stringify(auth?.userState);
+      if (storageString !== contextString) {
+        storage.setItem('userState', contextString);
+      }
+      return;
+    }
+    if (storageString) {
+      const storageState = JSON.parse(storageString);
+      auth?.login(storageState.user, storageState.token);
+    }
+  }, [auth?.userState]);
+
   const onLogout = () => {
     auth?.logout();
+    storage.removeItem('userState');
   };
 
   if (auth?.userState.isLogin) {
