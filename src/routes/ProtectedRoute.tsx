846cc62f66859eee7e200f5052167d17fa156057
sujikim <sujikim@student.42seoul.kr>
2022-08-18 19:52:12 +0900

    fix: ProtectedRoute prop 타입 분리
    통일성을 위해 props 인터페이스를 추가했습니다.

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -3,12 +3,17 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { userContext } from 'contexts/userContext';
 import toast from 'react-hot-toast';
 
-const ProtectedRoute = ({ redirectPath }: { redirectPath: string }) => {
+interface ProtectedRouteProp {
+  redirectPath: string;
+}
+
+const ProtectedRoute = ({ redirectPath }: ProtectedRouteProp) => {
   const auth = useContext(userContext);
+
   if (auth?.userState.isLogin) {
     return <Outlet />;
   }
-  toast.error("로그인 후 이용해주세요")
+  toast.error('로그인 후 이용해주세요');
   return <Navigate to={redirectPath} />;
 };
 
