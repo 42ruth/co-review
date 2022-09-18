13d1d060651c4cf55aaad059f40f87d5e1803480
Soohyun Choi <djm05190@gmail.com>
2022-09-18 16:19:00 +0900

    fix: 로그인 했다면 메인페이지로 리다이렉트

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -3,11 +3,15 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useCheckLogin } from 'hooks/useCheckLogin';
 
 interface ProtectedRouteProp {
+  isReverse?: boolean;
   redirectPath: string;
 }
 
-const ProtectedRoute = ({ redirectPath }: ProtectedRouteProp) => {
-  if (useCheckLogin()) {
+const ProtectedRoute = ({
+  isReverse = false,
+  redirectPath,
+}: ProtectedRouteProp) => {
+  if (useCheckLogin() !== isReverse) {
     return <Outlet />;
   }
   return <Navigate to={redirectPath} replace />;
