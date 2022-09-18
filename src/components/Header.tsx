81c6ce00f6e9964150ebbb04b7438111cc42734b
sujikim <sujikim@student.42seoul.kr>
2022-09-18 15:37:29 +0900

    add: 로그인하지 않았을 때 리뷰관련 버튼 로그인페이지로 연결
    리뷰신청하기 버튼과 리뷰하러가기버튼 클릭시 로그인여부을 확인하고
    로그인하지 않았을 때 로그인페이지로 연결합니다
    
    Resolves #131

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import 'assets/css/Header.css';
 import MyPageButton from 'components/MyPage/MyPageButton';
+import { useCheckLogin } from 'hooks/useCheckLogin';
 
 const Header = () => {
+  const isLogged = useCheckLogin();
   return (
     <header>
       <Link to="/">LOGO</Link>
-      <Link to="/editor" className="button">
+      <Link to={isLogged ? '/editor' : 'login'} className="button">
         리뷰 신청하기
       </Link>
       <Link to="/login">로그인페이지</Link>
-      <MyPageButton />
+      {isLogged && <MyPageButton />}
     </header>
   );
 };
